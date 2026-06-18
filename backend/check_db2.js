dd1e06266b293d312d3914e200834a44a923da8d
Benny <bennyivan14@gmail.com>
2026-06-18 15:41:03 +0300

    chore: remove unnecessary debug, test, and redundant report files from repository

--- backend/check_db2.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/genz_db').then(async () => {
-  const db = mongoose.connection.db;
-  const msgs = await db.collection('messages').find({ $or: [{ mediaUrl: { $exists: true, $ne: "" } }, { content: /uploads/ }] }).limit(5).toArray();
-  console.log(JSON.stringify(msgs, null, 2));
-  process.exit(0);
-}).catch(console.error);
--- backend/check_db4.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/tm-whatsapp').then(async () => {
-  const db = mongoose.connection.db;
-  const msgs = await db.collection('messages').find({ messageType: { $ne: 'text' } }).limit(5).toArray();
-  console.log(JSON.stringify(msgs, null, 2));
-  process.exit(0);
-}).catch(console.error);
