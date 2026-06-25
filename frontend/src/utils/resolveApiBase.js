03c2318de69193f0ea34f02abf0194db4f75bb24
Benny <bennyivan14@gmail.com>
2026-06-25 03:46:38 +0300

    fix: export resolveSocketOrigin for production build

--- frontend/src/utils/resolveApiBase.js
+++ frontend/src/utils/resolveApiBase.js
@@ -3,4 +3,8 @@ export const resolveApiBase = () => {
   return (import.meta.env.VITE_API_URL || '').replace(/\/$/, '');
 };
 
+export const resolveSocketOrigin = () => {
+  return (import.meta.env.VITE_SOCKET_URL || import.meta.env.VITE_API_URL?.replace(/\/api$/, '') || '').replace(/\/$/, '');
+};
+
 export default resolveApiBase;
